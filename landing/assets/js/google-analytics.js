2016e66368690906725dc4f295fb71c939037cbc
bkillenit <blaine.j.killen@gmail.com>
2013-04-22 16:30:49 -0400

    made a noob move with the google analytics js by including script tags in a js file, please forgive

--- landing/assets/js/google-analytics.js
+++ landing/assets/js/google-analytics.js
@@ -1,12 +1,12 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40000925-1']);
+_gaq.push(['_setDomainName', 'checkit.co']);
+_gaq.push(['_trackPageview']);
 
-  ga('create', 'UA-40000925-1', 'checkit.co');
-  ga('send', 'pageview');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  _gaq.push(['_setLocalGifPath', 'http://checkit.co/__utm.gif']);
-  _gaq.push(['_setLocalRemoteServerMode']);
-</script>
+ga('create', 'UA-40000925-1', 'checkit.co');
+ga('send', 'pageview');
